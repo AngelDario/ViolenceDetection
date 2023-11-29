26da93cadefd6edfb220a53c33cd744c4f442242
Angel Diaz <37408785+AngelDario@users.noreply.github.com>
2023-11-28 20:51:51 -0500

    Update preprocess.py

--- preprocess.py
+++ preprocess.py
@@ -3,10 +3,18 @@ import tqdm
 import os
 
 videos = glob.glob("DATASET/*/*/*.avi", recursive=True)
+begin_path = "/home/angeldiaz/preprocess/"
+
+
+#os.system(f'cd ../openpose && ./build/examples/openpose/openpose.bin --video /home/angeldiaz/preprocess/DATASET/train/Fight/_2RYnSFPD_U_0.avi --display 0 --write_video /home/angeldiaz/preprocess/_2RYnSFPD_U_0.avi --disable_blending > /dev/null --net_resolution 160x80')
 
 for video in tqdm.tqdm(videos):
     
     videoOpenPose = video.replace("DATASET", "DATASET_OPENPOSE")
+    save_path = os.path.dirname(videoOpenPose)
 
-    
-    os.system(f'cd ../openpose && ./build/examples/openpose/openpose.bin --video {video} --display 0 --write_video {videoOpenPose} --disable_blending > /dev/null')
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    tqdm.tqdm.write("Processing: " + video)
+    os.system(f'cd ../openpose && ./build/examples/openpose/openpose.bin --video {begin_path + video} --display 0 --write_video {begin_path + videoOpenPose} --disable_blending > /dev/null --net_resolution 160x80')
